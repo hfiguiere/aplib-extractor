89c855c42ce79236fae6c72860f261afa9bf4a22
Hubert Figuière <hub@figuiere.net>
2016-03-23 00:49:14 -0400

    Dumper tool has a (non functional) audit command

--- src/bin/dumper.rs
+++ src/bin/dumper.rs
@@ -9,6 +9,7 @@ extern crate docopt;
 extern crate rustc_serialize;
 
 use docopt::Docopt;
+use rustc_serialize::{Decodable, Decoder};
 
 use aplib::AplibObject;
 use aplib::Library;
@@ -17,11 +18,23 @@ use aplib::StoreWrapper;
 
 const USAGE: &'static str = "
 Usage:
-  dumper ([--all] | [--albums] [--versions] [--masters] [--folders] [--keywords]) <path>
+  dumper <command> ([--all] | [--albums] [--versions] [--masters] [--folders] [--keywords]) <path>
+
+Options:
+    --all          Select all objects
+    --albums       Select only albums
+    --masters      Select only masters
+    --folders      Select only folders
+    --keywords     Select only keywords
+
+Commands are:
+    dump           Dump the objects
+    audit          Audit mode: output what we ignored
 ";
 
 #[derive(Debug, RustcDecodable)]
 struct Args {
+    arg_command: Command,
     arg_path: String,
     flag_all: bool,
     flag_albums: bool,
@@ -29,6 +42,53 @@ struct Args {
     flag_masters: bool,
     flag_folders: bool,
     flag_keywords: bool
+}
+
+#[derive(Debug)]
+enum Command {
+    Dump,
+    Audit,
+    Unknown(String)
+}
+
+impl Decodable for Command {
+    fn decode<D: Decoder>(d: &mut D) -> Result<Command, D::Error> {
+        let s = try!(d.read_str());
+        Ok(match &*s {
+            "dump" => Command::Dump,
+            "audit" => Command::Audit,
+            s => Command::Unknown(s.to_string()),
+        })
+    }
+}
+
+fn main() {
+
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| d.decode())
+        .unwrap_or_else(|e| e.exit());
+    {
+        match args.arg_command {
+            Command::Dump =>
+                process_dump(&args),
+            Command::Audit =>
+                process_audit(&args),
+            _ =>
+                ()
+        };
+    }
+}
+
+
+fn process_audit(args: &Args) {
+
+    let mut library = Library::new(&args.arg_path);
+
+    library.load_folders();
+    library.load_albums();
+    library.load_masters();
+    library.load_versions();
+
 }
 
 /// print the keywords with indentation for the hierarchy
@@ -48,109 +108,103 @@ fn print_keywords(keywords: &Vec<Keyword>, indent: &String) {
     }
 }
 
-fn main() {
-
-    let args: Args = Docopt::new(USAGE)
-        .and_then(|d| d.decode())
-        .unwrap_or_else(|e| e.exit());
+fn process_dump(args: &Args) {
+    let mut library = Library::new(&args.arg_path);
 
     {
-        let mut library = Library::new(&args.arg_path);
+        let version = library.library_version();
+        println!("Version {}", version);
+    }
 
-        {
-            let version = library.library_version();
-            println!("Version {}", version);
-        }
+    let model_info = library.get_model_info();
 
-        let model_info = library.get_model_info();
+    println!("model info");
+    println!("\tDB version: {}", model_info.db_version);
+    println!("\tDB minor version: {}", model_info.db_minor_version);
+    println!("\tProject version: {}", model_info.project_version);
 
-        println!("model info");
-        println!("\tDB version: {}", model_info.db_version);
-        println!("\tDB minor version: {}", model_info.db_minor_version);
-        println!("\tProject version: {}", model_info.project_version);
-
-        library.load_folders();
-        library.load_albums();
-        if args.flag_all || args.flag_folders {
-            let folders = library.get_folders();
-            println!("{} Folders:", folders.len());
-            println!("| Name | uuid | type | model id | path |");
-            for folder_uuid in folders {
-                if folder_uuid.is_empty() {
-                    continue;
-                }
-                match library.get(folder_uuid) {
-                    Some(&StoreWrapper::Folder(ref folder)) =>
-                        println!("| {} | {} | {} | {} | {} |",
-                                 folder.name, folder.uuid(),
-                                 folder.folder_type,
-                                 folder.model_id(), folder.path),
-                    _ => println!("folder {} not found", folder_uuid)
-                }
+    library.load_folders();
+    library.load_albums();
+    if args.flag_all || args.flag_folders {
+        let folders = library.get_folders();
+        println!("{} Folders:", folders.len());
+        println!("| Name | uuid | impl album | type | model id | path |");
+        for folder_uuid in folders {
+            if folder_uuid.is_empty() {
+                continue;
+            }
+            match library.get(folder_uuid) {
+                Some(&StoreWrapper::Folder(ref folder)) =>
+                    println!("| {} | {} | {} | {} | {} | {} |",
+                             folder.name, folder.uuid(),
+                             folder.implicit_album_uuid,
+                             folder.folder_type,
+                             folder.model_id(), folder.path),
+                _ => println!("folder {} not found", folder_uuid)
             }
         }
-        if args.flag_all || args.flag_albums {
-            let albums = library.get_albums();
-            println!("{} Albums:", albums.len());
-            println!("| name | uuid | folder | type | class | model id |");
-            for album_uuid in albums {
-                if album_uuid.is_empty() {
-                    continue;
-                }
-                match library.get(album_uuid) {
-                    Some(&StoreWrapper::Album(ref album)) =>
-                        println!("| {} | {} | {} | {} | {} | {} |",
-                                 album.name,
-                                 album.uuid(), album.parent(),
-                                 album.album_type,
-                                 album.subclass, album.model_id()),
-                    _ => println!("album {} not found", album_uuid)
-                }
+    }
+    if args.flag_all || args.flag_albums {
+        let albums = library.get_albums();
+        println!("{} Albums:", albums.len());
+        println!("| name | uuid | parent (fldr) | query (fldr) | type | class | model id |");
+        for album_uuid in albums {
+            if album_uuid.is_empty() {
+                continue;
+            }
+            match library.get(album_uuid) {
+                Some(&StoreWrapper::Album(ref album)) =>
+                    println!("| {} | {} | {} | {} | {} | {} | {} |",
+                             album.name,
+                             album.uuid(), album.parent(),
+                             album.query_folder_uuid,
+                             album.album_type,
+                             album.subclass, album.model_id()),
+                _ => println!("album {} not found", album_uuid)
             }
         }
-        if args.flag_all || args.flag_keywords {
-            let keywords = library.list_keywords();
-            println!("{} keywords:", keywords.len());
-            println!("| uuid | parent | name |");
-            print_keywords(&keywords, &"".to_string());
-        }
+    }
+    if args.flag_all || args.flag_keywords {
+        let keywords = library.list_keywords();
+        println!("{} keywords:", keywords.len());
+        println!("| uuid | parent | name |");
+        print_keywords(&keywords, &"".to_string());
+    }
 
-        library.load_masters();
-        library.load_versions();
-        if args.flag_all || args.flag_masters  {
-            let masters = library.get_masters();
-            println!("{} Masters:", masters.len());
-            println!("| uuid | project | path |");
-            for master_uuid in masters {
-                if master_uuid.is_empty() {
-                    continue;
-                }
-                match library.get(master_uuid) {
-                    Some(&StoreWrapper::Master(ref master)) =>
-                        println!("| {} | {} | {} |",
-                                 master.uuid(), master.parent(),
-                                 master.image_path),
-                    _ => println!("master {} not found", master_uuid)
-                }
+    library.load_masters();
+    library.load_versions();
+    if args.flag_all || args.flag_masters  {
+        let masters = library.get_masters();
+        println!("{} Masters:", masters.len());
+        println!("| uuid | project | path |");
+        for master_uuid in masters {
+            if master_uuid.is_empty() {
+                continue;
             }
-
+            match library.get(master_uuid) {
+                Some(&StoreWrapper::Master(ref master)) =>
+                    println!("| {} | {} | {} |",
+                             master.uuid(), master.parent(),
+                             master.image_path),
+                _ => println!("master {} not found", master_uuid)
+            }
         }
-        if args.flag_all || args.flag_versions  {
-            let versions = library.get_versions();
-            println!("{} Versions:", versions.len());
-            println!("| uuid | master | project | name | original |");
-            for version_uuid in versions {
-                if version_uuid.is_empty() {
-                    continue;
-                }
-                match library.get(version_uuid) {
-                    Some(&StoreWrapper::Version(ref version)) =>
-                                println!("| {} | {} | {} | {} | {} |",
-                                         version.uuid(), version.parent(),
-                                         version.project_uuid, version.name,
-                                         version.is_original),
-                    _ => println!("version {} not found", version_uuid)
-                }
+    }
+    if args.flag_all || args.flag_versions  {
+        let versions = library.get_versions();
+        println!("{} Versions:", versions.len());
+        println!("| uuid | master | project | name | original |");
+        for version_uuid in versions {
+            if version_uuid.is_empty() {
+                continue;
+            }
+            match library.get(version_uuid) {
+                Some(&StoreWrapper::Version(ref version)) =>
+                    println!("| {} | {} | {} | {} | {} |",
+                             version.uuid(), version.parent(),
+                             version.project_uuid, version.name,
+                             version.is_original),
+                _ => println!("version {} not found", version_uuid)
             }
         }
     }
